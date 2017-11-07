492bf820ecdc640a9398ade4ed8fa847b460320e
Arnaud Manaranche <arnaud.mana@gmail.com>
2017-11-07 16:02:39 +0100

    display post input

--- Sources/KituraTest/main.swift
+++ Sources/KituraTest/main.swift
@@ -1,12 +1,17 @@
 import Kitura
+import KituraMarkdown
 
 // Create a new router
 let router = Router()
 
+router.all("/", middleware: BodyParser())
+
+router.add(templateEngine: KituraMarkdown())
+
 // Handle HTTP GET requests to /
 router.get("/") {
     request, response, next in
-    response.send("Hello World")
+    try response.render("/docs/index.md", context: [String:Any]())
     next()
 }
 
@@ -15,6 +20,27 @@ router.get("/hello/:name") {
     request, response, next in
     let name = request.parameters["name"] ?? "Platymus"
     try response.send("Hello \(name)").end()
+    next()
+}
+
+router.post("/") {
+    request, response, next in
+    
+    guard let body = request.body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    guard case .urlEncoded(let data) = body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    if let text = data["champ"] {
+        try response.send(text).end()
+    } else {
+        try response.send("nok").end()
+    }
     next()
 }
 
---
+++ views/docs/index.md
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Web Service Kitura</title>
+</head>
+
+<body>
+    <form action='/' method='post'>
+        <input type='text' placeholder='Mon Texte' name='champ' />
+        <input type='text' placeholder='Mon Texte' name='champ2' />
+        <button type='submit'>Envoyer</button>
+    </form>
+</body>
+
+</html>
