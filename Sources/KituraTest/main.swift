4ea3349243bace8dbfe3144a5933c70fa784a635
Arnaud Manaranche <arnaud.mana@gmail.com>
2017-11-16 20:59:12 +0100

    chatroom begin + session

--- Sources/KituraTest/main.swift
+++ Sources/KituraTest/main.swift
@@ -1,47 +1,104 @@
 import Kitura
-import KituraMarkdown
+import KituraStencil
+import KituraSession
+import Foundation
+import SwiftyJSON
+
+// Struc User
+struct User {
+    var pseudo:String
+}
+
+// Struc Message
+struct Message {
+    var content:String
+    var expediteur:User
+}
+
+var users = [User]()
+var messages = [Message]()
+
+// Where we will store the current session data
+var sess: SessionState?
+
+// Initialising our KituraSession
+let session = Session(secret: "kitura_session")
 
 // Create a new router
 let router = Router()
 
-router.all("/", middleware: BodyParser())
+router.all("/", middleware: BodyParser(), StaticFileServer(path: "./Public"), session)
 
-router.add(templateEngine: KituraMarkdown())
-
-// Handle HTTP GET requests to /
-router.get("/") {
-    request, response, next in
-    try response.render("/docs/index.md", context: [String:Any]())
-    next()
-}
+router.setDefault(templateEngine: StencilTemplateEngine())
 
 // Handle HTTP GET requests to /hello/:user
-router.get("/hello/:name") {
-    request, response, next in
-    let name = request.parameters["name"] ?? "Platymus"
-    try response.send("Hello \(name)").end()
+router.get("/") { request, response, next in
+    
+    //Again get the current session
+    sess = request.session
+    
+    //Check if we have a session and it has a value for email
+    if let sess = sess, let pseudo = sess["pseudo"].string {
+        try response.render("index", context: ["users": users, "messages": messages]).end()
+    } else {
+        try response.render("index", context: ["users": users]).end()
+    }
+    
     next()
 }
 
-router.post("/") {
+router.post("/") { request, response, next in
+    
+    if let body = request.body {
+        
+        switch body {
+        case .urlEncoded(let params):
+            let content = params["content"] ?? ""
+            let pseudo = sess!["pseudo"].string ?? ""
+            let expediteur = User(pseudo: pseudo)
+            
+            users.append(User(pseudo: pseudo))
+            messages.append(Message(content: content, expediteur: expediteur))
+            
+            try response.render("room", context: ["users": users, "messages": messages]).end()
+        default:
+            try response.redirect("/error").end()
+        }
+    }
+    
+    next()
+}
+
+router.post("/room") { request, response, next in
+    
+    //Get current session
+    sess = request.session
+    
+    var maybePseudo: String?
+    
+    switch request.body {
+    case .urlEncoded(let params)?:
+        maybePseudo = params["pseudo"]
+    default: break
+    }
+    
+    if let pseudo = maybePseudo, let sess = sess {
+        sess["pseudo"] = JSON(pseudo)
+        try response.render("room", context: ["user": "pseudo"]).end()
+    }
+    
+}
+
+router.get("/logout") {
     request, response, next in
     
-    guard let body = request.body else {
-        try response.status(.badRequest).end()
-        return
+    //Destroy all data in our session
+    sess?.destroy() {
+        (error: NSError?) in
+        if let error = error {
+        }
     }
-    
-    guard case .urlEncoded(let data) = body else {
-        try response.status(.badRequest).end()
-        return
-    }
-    
-    if let text = data["champ"] {
-        try response.send(text).end()
-    } else {
-        try response.send("nok").end()
-    }
-    next()
+    try response.render("index", context: ["users": users, "messages": messages]).end()
 }
 
 // Add an HTTP server and connect it to the router
