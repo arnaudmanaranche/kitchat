e667257d05fd6fdd449f41d1ca080334da7c701d
Arnaud Manaranche <arnaud.mana@gmail.com>
2017-11-29 10:58:23 +0100

    wip

--- Sources/KituraTest/main.swift
+++ Sources/KituraTest/main.swift
@@ -13,67 +13,118 @@ router.all("/", middleware: BodyParser(), StaticFileServer(path: "./Public"))
 router.setDefault(templateEngine: StencilTemplateEngine())
 
 // Class Users
-class Grades : Table {
-    let tableName = "grades"
-    let key = Column("key")
-    let course = Column("course")
-    let grade = Column("grade")
-    let studentId = Column("studentId")
+class Users : Table {
+    let tableName = "users"
+    let pseudo = Column("pseudo", Varchar.self, length: 50)
+    let password = Column("password", Varchar.self, length: 50)
 }
 
-let grades = Grades()
+let users = Users()
 
 let connection = PostgreSQLConnection(host: "localhost", port: 5432, options: [.databaseName("kitchat")])
 
-func grades(_ callback:@escaping (String)->Void) -> Void {
+connection.connect { (error) in
+    print(error as Any)
+    users.create(connection: connection) { (result) in
+        print(result)
+    }
+    connection.closeConnection()
+}
+
+router.get("/") { request, response, next in
     connection.connect() { error in
-        if let error = error {
-            callback("Error is \(error)")
+        if error != nil {
+            print("nok")
             return
         }
         else {
-            // Build and execute your query here.
-            
-            // First build query
-            let query = Select(grades.course, grades.grade, from: grades)
+            let query = Select(users.pseudo, from: users)
             
             connection.execute(query: query) { result in
                 if let resultSet = result.asResultSet {
                     var retString = ""
                     
-                    for title in resultSet.titles {
-                        // The column names of the result.
-                        retString.append("\(title.padding(toLength: 35, withPad: " ", startingAt: 0))")
-                    }
-                    retString.append("\n")
-                    
                     for row in resultSet.rows {
                         for value in row {
                             if let value = value {
                                 let valueString = String(describing: value)
-                                retString.append("\(valueString.padding(toLength: 35, withPad: " ", startingAt: 0))")
+                                retString.append("\(valueString)")
                             }
                         }
                         retString.append("\n")
                     }
-                    callback(retString)
+                    do {
+                        try response.render("index", context: ["users": retString]).end()
+                    }
+                        catch {
+                        print("error")
+                    }
                 }
-                else if let queryError = result.asError {
-                    // Something went wrong.
-                    callback("Something went wrong \(queryError)")
+                else if result.asError != nil {
+                    print("nok")
                 }
             }
         }
     }
 }
 
-router.get("/") {
-    request, response, next in
+router.post("/login") { request, response, next in
     
-    grades() {
-        resp in
-        response.send(resp)
-        next()
+    guard let body = request.body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    guard case .urlEncoded(let data) = body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    let pseudo = data["pseudo1"]
+    let password = data["password1"]
+    
+    connection.connect() { error in
+        if error != nil {
+            print("nok")
+            return
+        }
+        else {
+            let query = Select(users.pseudo, users.password, from: users).where(users.pseudo == pseudo! && users.password == password!)
+            
+            connection.execute(query: query) { result in
+                print(query)
+            }
+        }
+    }
+}
+
+router.post("/signin") { request, response, next in
+    
+    guard let body = request.body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    guard case .urlEncoded(let data) = body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    let pseudo = data["pseudo"]
+    let password = data["password"]
+    
+    connection.connect() { error in
+        if error != nil {
+            print("nok")
+            return
+        }
+        else {
+            let query = Insert(into: users, values: pseudo, password)
+            
+            connection.execute(query: query) { result in
+                print(query)
+            }
+        }
     }
 }
 
